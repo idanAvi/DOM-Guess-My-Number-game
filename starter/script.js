b3079d7eaa626f68a6cc0df680641450743fd1b6
idanAvi <idanaviad1@gmail.com>
2022-08-25 21:02:19 +0300

    temp change

--- starter/script.js
+++ starter/script.js
@@ -53,7 +53,7 @@ document.querySelector('.check').addEventListener('click', function () {
       }
       document.querySelector('body').style.backgroundColor = '#60b347';
       displayMassage('Correct number!');
-      score = 0;
+      score = 1;
     }
   }
 });
